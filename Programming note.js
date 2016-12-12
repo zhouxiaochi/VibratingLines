f9f411664999e7461cc16a10ad9bd25806b89f25
zhouxiaochi <jimmyzhou.ntu@gmail.com>
2016-12-12 21:00:33 +0800

    Update Programming note.js

--- Programming note.js
+++ Programming note.js
@@ -1,1 +1,26 @@
+//-------------How to play with sound in p5.js
+sound = loadSound("music_name");// load a sound
+sound.setVolume(10)// set the volume of the sound
+
+amplitude = new p5.Amplitude();// create a Amplitude instance
+
+//-------------How to draw things in p5.js
+storkeWeight(4)// set the stroke weight
+line(0,0,0,5)// draw a vertical line that is 5px long
+
+
+
+//-------------How to deal with HTML UI components
+input = creatInput();
+input.addClass("input");// input is a piece of stype code in HTML
+
+button_save = document.getElementById('button_SAVE');// link button_save with a button in HTML which id is "button_SAVE"
+button_save.addEventListener("click", storetheshape);// add onclick listener with function storetheshape;
+
+
+
+
+
+
+
 
